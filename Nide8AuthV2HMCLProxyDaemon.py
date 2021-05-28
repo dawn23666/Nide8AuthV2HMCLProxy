6c76d5a5ca7f6cbc7b86b30fa2dc1f82b490a99f
dawn <dawn@katwczn.com>
2021-05-29 01:00:05 +0800

    程序结束响应

--- Nide8AuthV2HMCLProxyDaemon.py
+++ Nide8AuthV2HMCLProxyDaemon.py
@@ -1,7 +1,13 @@
-import os,time
+import os,time,sys
 a = 1
-while a == 1:
-    os.system('java -jar ./Nide8AuthV2HMCLProxy.jar &')
-    time.sleep(600)
+def kill_Nide8AuthV2HMCLProxy():
     pid = os.popen('ps -ef | grep Nide8AuthV2HMCLProxy').readlines()[0].split()[1]
     os.system('kill -9 '+pid)
+while a == 1:
+    try:
+        os.system('java -jar ./Nide8AuthV2HMCLProxy.jar &')
+        time.sleep(600)
+        kill_Nide8AuthV2HMCLProxy()
+    except KeyboardInterrupt:
+        kill_Nide8AuthV2HMCLProxy()
+        sys.exit()

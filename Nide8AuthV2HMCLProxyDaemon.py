a757fa0017b9ee1c1da777a8ea9e4aa74e5e3666
dawn <dawn@katwczn.com>
2021-05-29 12:15:24 +0800

    使用crontab执行定时任务

--- Nide8AuthV2HMCLProxyDaemon.py
+++ Nide8AuthV2HMCLProxyDaemon.py
@@ -1,13 +1,7 @@
-import os,time,sys
-a = 1
-def kill_Nide8AuthV2HMCLProxy():
+import os,sys
+try:
     pid = os.popen('ps -ef | grep Nide8AuthV2HMCLProxy').readlines()[0].split()[1]
     os.system('kill -9 '+pid)
-while a == 1:
-    try:
-        os.system('java -jar ./Nide8AuthV2HMCLProxy.jar &')
-        time.sleep(600)
-        kill_Nide8AuthV2HMCLProxy()
-    except KeyboardInterrupt:
-        kill_Nide8AuthV2HMCLProxy()
-        sys.exit()
+except:
+    pass
+os.system('java -jar ./Nide8AuthV2HMCLProxy.jar &')
